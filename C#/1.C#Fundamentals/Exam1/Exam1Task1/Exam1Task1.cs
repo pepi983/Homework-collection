463a9765637448bc8db30e7a7d0c4b3ead32987c
pepi983 <pepi983@abv.bg>
2016-04-12 20:46:25 +0300

    Added a few more solutions to homework 4 and fixed exam 1 solution

--- C#/1.C#Fundamentals/Exam1/Exam1Task1/Exam1Task1.cs
+++ C#/1.C#Fundamentals/Exam1/Exam1Task1/Exam1Task1.cs
@@ -6,21 +6,31 @@ namespace Exam1Task1
     {
         static void Main()
         {
-            int a = int.Parse(Console.ReadLine());
-            int b = int.Parse(Console.ReadLine());
-            int c = int.Parse(Console.ReadLine());
-            int Max1 = Math.Max(a, b);
-            int Max2 = Math.Max(Max1, c);
-            int Max = Math.Max(Max1, Max2);
-            int Min1 = Math.Min(a, b);
-            int Min2 = Math.Min(Min1, c);
-            int Min = Math.Min(Min1, Min2);
-            int arithmetic = (a + b + c) / 3;
-            Console.WriteLine(Max);
-            Console.WriteLine(Min);
-            Console.WriteLine(arithmetic);
-
-        }   
-
+            long A = long.Parse(Console.ReadLine());
+            long B = long.Parse(Console.ReadLine());
+            long C = long.Parse(Console.ReadLine());
+            double arithmetic = ((A + B + C) / 3.0);
+            long Max1 = Math.Max(A, B);
+            long Max2 = Math.Max(Max1, C);
+            long Min1 = Math.Min(A, B);
+            long Min2 = Math.Min(Min1, C);
+            if (Max1 > Max2)
+            {
+                Console.WriteLine(Max1);
+            }
+            else
+            {
+                Console.WriteLine(Max2);
+            }
+            if (Min1 < Min2)
+            {
+                Console.WriteLine(Min1);
+            }
+            else
+            {
+                Console.WriteLine(Min2);
+            }
+            Console.WriteLine("{0:F2}", arithmetic);
+        }
     }
 }
