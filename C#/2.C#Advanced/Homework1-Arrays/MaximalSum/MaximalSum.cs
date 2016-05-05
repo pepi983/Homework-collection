4ea934af1e0615ca301ed4acabd78c7b6ab9eb0a
pepi983 <pepi983@abv.bg>
2016-05-05 12:34:05 +0300

    added 1 more solution to homework 1 in c#advanced.

---
+++ C#/2.C#Advanced/Homework1-Arrays/MaximalSum/MaximalSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int arraySize = int.Parse(Console.ReadLine());
+        int[] array = new int[arraySize];
+        int currentSum = 0;
+        int maxSum = int.MinValue;
+
+
+        for (int i = 0; i < arraySize; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+
+            currentSum += array[i];
+            if (currentSum < array[i])
+            {
+                currentSum = array[i];
+            }
+
+            if (currentSum > maxSum)
+            {
+                maxSum = currentSum;
+            }
+        }
+        Console.WriteLine(maxSum);
+    }
+}
+
+// reshenie ot foruma.
---
+++ C#/2.C#Advanced/Homework1-Arrays/SortArray/SortArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumOfKMembers
+{
+    class SumOfKNumbers
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] numbers = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(numbers);
+            for (int l = 0; l < N; l++)
+            {
+                Console.WriteLine(numbers[l]); 
+            }
+        }
+    }
+}
