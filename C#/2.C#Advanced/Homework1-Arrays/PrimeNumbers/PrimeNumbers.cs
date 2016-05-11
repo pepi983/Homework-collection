1a5ecd4d46345250bbb9adcaa1a31502426943e8
Peter Petrov <Peter Petrov>
2016-05-11 14:19:15 +0300

    idk

---
+++ C#/2.C#Advanced/Homework1-Arrays/PrimeNumbers/PrimeNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SumOfKMembers
+{
+    class SumOfKNumbers
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] numbers = new int[N+1];
+            bool isPrime = true;
+            int maxPrime = 0;
+            for (int l = 1; l <= N; l++)
+            {
+                numbers[l] = l;
+                if (numbers[l] < 2)
+                {
+                    isPrime = false;
+                }
+                else
+                {
+                    for (int i = 2; i <= (int)Math.Sqrt(l); i++)
+                    {
+                        if (numbers[l] % i == 0)
+                        {
+                            isPrime = false;
+                        }
+                        else
+                        {
+                            maxPrime = numbers[l];
+                        }
+
+                    }
+                }
+            }
+                          
+            Console.WriteLine(maxPrime);
+        }
+    }
+}
