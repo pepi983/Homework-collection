f4fb21e4a8ac90959ca46638233e4fc9e43a7709
Peter Petrov <Peter Petrov>
2016-05-17 00:17:16 +0300

    Added full solution to homework5 in c# advanced.

---
+++ C#/2.C#Advanced/Homework5-ClassesAndObjects/TriangleOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TriangleSideAndAltitude
+{
+    class TriangleOne
+    {
+        static void Main(string[] args)
+        {
+            double side = double.Parse(Console.ReadLine());
+            double altitude = double.Parse(Console.ReadLine());
+            double area = (side * altitude) / 2.0;
+            Console.WriteLine("{0:F2}", area);
+        }
+    }
+}
---
+++ C#/2.C#Advanced/Homework5-ClassesAndObjects/TriangleTwo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Triangle2
+{
+    class TriangleTwo
+    {
+        static void Main()
+        {
+            double side1 = double.Parse(Console.ReadLine());
+            double side2 = double.Parse(Console.ReadLine());
+            double side3 = double.Parse(Console.ReadLine());
+            double p = (side1 + side2 + side3) / 2.0;
+            double kur1 = p - side1;
+            double kur2 = p - side2;
+            double kur3 = p - side3;
+            double almostresult = p * kur1 * kur2 * kur3;
+            double area = Math.Sqrt((double)almostresult);
+            Console.WriteLine("{0:F2}", area); // tree da e 6i neshto  
+        }
+    }
+}
